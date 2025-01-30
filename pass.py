4001ac0a102d081ced7824ed3065e1b832dad524
Joyce Wambui <joycewambui@gmail.com>
2025-01-30 15:25:38 +0300

    created a pass file

---
+++ pass.py
@@ -0,0 +1,6 @@
+countries = ['Congo', 'Japan', 'Germany', 'Kenya', 'Liberia', 'Morocco', 'Nigeria', 'Libya']
+for country in countries:
+    if 'e' in country.lower():
+        break
+    else:
+        print(country)
